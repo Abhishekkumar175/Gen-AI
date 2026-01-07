0366009c812e41f641d448bca80003a99fe0faf0
abhishekkumar <abhishekkumar345ssm@gmail.com>
2026-01-07 15:42:34 +0530

    gemini-api-testing

---
+++ 02_gemini_api/test.py
@@ -0,0 +1,13 @@
+from dotenv import load_dotenv
+from google import genai
+
+# Load environment variables from .env file
+load_dotenv()
+
+# The client gets the API key from the environment variable `GEMINI_API_KEY`.
+client = genai.Client()
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash", contents="Explain how AI works in a few words in 100 words."
+)
+print(response.text)
